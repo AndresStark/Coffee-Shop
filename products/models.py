b5ad1575ce6c245d6f075d9c45472821299014d6
Andrés Sánchez <cloud@DESKTOP-1TQN06K>
2025-04-09 22:56:21 -0600

    Admin created and configurated. Creation date field added to Products model, it is recommended to change the photos save file

--- products/models.py
+++ products/models.py
@@ -6,6 +6,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="precio")
     available = models.BooleanField(default=True, verbose_name="disponible")
     photo = models.ImageField(upload_to="logos", null=True, blank=True, verbose_name="foto")
+    creation_date = models.DateField(null=True, auto_created=True, verbose_name="fecha_de_creacion")
 
     def __str__(self):
         return self.name
